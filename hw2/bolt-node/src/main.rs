4aa154ad5f08bfe79f045fa57932370307249f7d
Josh Bowden <josbow@gmail.com>
2020-10-19 01:14:14 -0500

    feat(pa2): snapshot of progress

--- hw2/bolt-node/src/main.rs
+++ hw2/bolt-node/src/main.rs
@@ -1,4 +1,162 @@
-fn main() {
-    println!("bolt - node");
+extern crate rmp_serde as rmps;
 
+use std::io;
+use std::io::prelude::*;
+use std::io::Cursor;
+
+use tokio::prelude::*;
+use tokio::net::TcpListener;
+use tokio::io::AsyncReadExt;
+use tokio::io::BufReader;
+use rmps::{Deserializer, Serializer};
+use bytes::BytesMut;
+
+use bolt::MessageHeader;
+use std::thread;
+
+#[derive(PartialEq)]
+enum BufferState {
+    Empty,
+    WaitHeader,
+    GotHeader,
+    WaitData,
+    Done,
+}
+
+#[derive(PartialEq)]
+struct BufferTarget {
+    header: MessageHeader,
+    header_len: u32,
+}
+
+impl BufferTarget {
+    fn data_len(&self) -> u32 {
+        self.header.length
+    }
+
+    fn buf_target(&self) -> u32 {
+        self.header_len + self.data_len()
+    }
+}
+
+struct BufferContext {
+    buffer: BytesMut,
+    header_with_target: Option<BufferTarget>,
+}
+
+impl BufferContext {
+    fn new(capacity: usize) -> BufferContext {
+        BufferContext {
+            buffer: BytesMut::with_capacity(capacity),
+            header_with_target: None,
+        }
+    }
+
+    fn header(&self) -> Option<&MessageHeader> {
+        self.header_with_target.as_ref().map(|x| &x.header)
+    }
+
+    fn set_header_with_len(&mut self, header: MessageHeader, header_len: u32) {
+        self.header_with_target = Some(BufferTarget { header, header_len });
+    }
+    
+    fn try_read_header(&mut self) -> Result<(), rmps::decode::Error> {
+        let mut cursor = Cursor::new(&self.buffer);
+        cursor.set_position(0);
+
+        let parse_result = rmps::from_read::<_, MessageHeader>(&mut cursor);
+        let result = match parse_result {
+            Ok(header) => {
+                let header_len = cursor.position() as u32;
+                self.set_header_with_len(header, header_len);
+                Ok(())
+            }
+            Err(err) => {
+                Err(err)
+            }
+        };
+
+        result
+    }
+
+    fn get_state(&self) -> (BufferState, u32) {
+        let pos = self.buffer.len() as u32;
+        let state = if pos == 0 {
+            BufferState::Empty
+        }
+        else { // pos > 0
+            if let Some(target) = &self.header_with_target {
+                if pos >= target.buf_target() {
+                    BufferState::Done
+                }
+                else {
+                    BufferState::WaitData
+                }
+            }
+            else {
+                BufferState::WaitHeader
+            }
+        };
+
+        (state, pos)
+    }
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listen_address: &'static str = "127.0.0.1:8080";
+    let mut listener = TcpListener::bind(listen_address).await?;
+    println!("listening on {}", listen_address);
+
+    thread::spawn(|| {
+        print!("> ");
+        io::stdout().flush().unwrap();
+
+        let stdin = io::stdin();
+        for line in stdin.lock().lines() {
+            println!("debug: got '{}'", line.unwrap());
+            print!("> ");
+            io::stdout().flush().unwrap();
+        }
+    });
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut ctx = BufferContext::new(8 * 1024);
+            let peer_addr = socket.peer_addr().map_or("(unknown)".into(), |addr| addr.to_string());
+
+            loop {
+                let (old_state, old_pos) = ctx.get_state();
+
+                let n = match socket.read_buf(&mut ctx.buffer).await {
+                    // socket closed
+                    Ok(n) if n == 0 => {
+                        println!("[{}] peer disconnected", peer_addr);
+                        return;
+                    }
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("[{}] error: failed to read from socket; err = {:?}", peer_addr, e);
+                        return;
+                    }
+                };
+
+                println!("[{}] read {} bytes", peer_addr, n);
+
+                if old_state == BufferState::Empty {
+                    ctx.try_read_header();
+                }
+
+                // Check if we have read the body at this point
+                let (new_state, _) = ctx.get_state();
+                if new_state == BufferState::Done {
+                    let target = ctx.header_with_target.as_ref().unwrap();
+                    println!("got data of {} bytes with message of kind {:?}", target.data_len(), target.header.kind)
+                }
+            }
+        });
+    }
 }
--- hw2/bolt/src/lib.rs
+++ hw2/bolt/src/lib.rs
@@ -1,7 +1,71 @@
+extern crate serde;
+extern crate rmp_serde as rmps;
+
+use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
+use rmps::{Deserializer, Serializer};
+
+pub mod message_kind {
+    const REQUEST_LISTING: &str = "LIST";
+    const RESPONSE_LISTING: &str = "FILES";
+    const REQUEST_FETCH: &str = "FETCH";
+    const STREAM_CHUNK: &str = "DATA";
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+pub struct MessageHeader {
+    pub kind: String,
+    pub length: u32,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+pub struct FileInfo {
+    name: String,
+    size: u64,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+pub struct FileFetchRequest {
+    name: String,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+pub struct FileChunkResponse {
+    path: String,
+    offset: u64,
+    size: u64,
+    buffer: Vec<u8>
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+struct FileListingRequest {
+    dummy: i32
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+struct FileListingResponse {
+    files: Vec<FileInfo>,
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::FileInfo;
+    use rmps::{Deserializer, Serializer};
+    use serde::{Deserialize, Serialize};
+    use std::io::Cursor;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let mut buf = Vec::new();
+        let expected = FileInfo {
+            name: "/foo.txt".into(),
+            size: 42,
+        };
+
+        expected.serialize(&mut Serializer::new(&mut buf)).unwrap();
+
+        let mut reader = Cursor::new(buf);
+        let actual = rmps::from_read::<_, FileInfo>(reader).unwrap();
+        assert_eq!(expected, actual);
     }
 }
